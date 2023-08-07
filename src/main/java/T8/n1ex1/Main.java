c0785bb42df123348d36ab9af289779944d0819b
Juanmoree <105069930+juanmoree@users.noreply.github.com>
2023-08-07 12:41:59 +0200

    Lambdas: Introducción
    Streams que incluyen la letra 'o'

---
+++ src/main/java/T8/n1ex1/Main.java
@@ -0,0 +1,19 @@
+package T8.n1ex1;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("murcielago", "hola", "probando", "1", "2", "Joan", "Hola mundo!", "prueba1", "prueba2");
+
+        list = list.stream()
+                // 'x' es el "consumidor"
+                .filter(x -> x.contains("o") || x.contains("O"))
+                .collect(Collectors.toList());
+
+        list.forEach(x -> System.out.println(x));
+    }
+}
