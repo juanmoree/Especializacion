6386b7142caaae1a020dca7ef2ca43da3c3419ab
Juanmoree <105069930+juanmoree@users.noreply.github.com>
2023-10-12 13:09:23 +0200

    Update tree

--- src/main/java/T6/n1ex1/Main.java
+++ src/main/java/T6/n1ex1/Main.java
@@ -4,14 +4,14 @@ public class Main {
 
     public static void main (String[] args){
 
-        Object uno = new Object();
-        Object dos = new Object();
-        Object tres = new Object();
+        String uno = "1";
+        String dos = "2";
+        String tres = "3";
 
-        NoGenericMethods prueba1 = new NoGenericMethods("juan", 3.5f, 1);
+        NoGenericMethods prueba1 = new NoGenericMethods(uno, dos, tres);
         NoGenericMethods prueba2 = new NoGenericMethods(dos, tres, uno);
 
         System.out.println(prueba1.getUno() + "\n" + prueba1.getDos() + "\n" + prueba1.getTres());
-        //System.out.println(prueba2.getDos());
+        System.out.println("Prueba dos" + "\n" + prueba2.getUno() + "\n" + prueba2.getDos() + "\n" + prueba2.getTres());
     }
 }
