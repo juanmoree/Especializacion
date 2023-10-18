c34a15414be80dfd358659a08e52cce317c588b7
Juanmoree <105069930+juanmoree@users.noreply.github.com>
2023-10-18 09:50:55 +0200

    Update tree

--- src/main/java/T6/n1ex1/Main.java
+++ src/main/java/T6/n1ex1/Main.java
@@ -14,4 +14,49 @@ public class Main {
         System.out.println(prueba1.getUno() + "\n" + prueba1.getDos() + "\n" + prueba1.getTres());
         System.out.println("Prueba dos" + "\n" + prueba2.getUno() + "\n" + prueba2.getDos() + "\n" + prueba2.getTres());
     }
+
+    public static class NoGenericMethods {
+        String uno = "uno";
+        String dos = "dos";
+        String tres = "tres";
+
+        public NoGenericMethods(String uno, String dos, String tres) {
+            this.uno = uno;
+            this.dos = dos;
+            this.tres = tres;
+        }
+
+        public Object getUno() {
+            return uno;
+        }
+
+        public void setUno(Object uno) {
+            this.uno = (String) uno;
+        }
+
+        public Object getDos() {
+            return dos;
+        }
+
+        public void setDos(Object dos) {
+            this.dos = (String) dos;
+        }
+
+        public Object getTres() {
+            return tres;
+        }
+
+        public void setTres(Object tres) {
+            this.tres = (String) tres;
+        }
+
+        @Override
+        public String toString() {
+            return "NoGenericMethods{" +
+                    "uno=" + uno +
+                    ", dos=" + dos +
+                    ", tres=" + tres +
+                    '}';
+        }
+    }
 }
